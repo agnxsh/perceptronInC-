328508098e8180b6c6ef426e89187726dcc458c2
Agnish Ghosh <iamagnix@gmail.com>
2022-09-04 10:25:02 +0530

    layers added

--- nncpp.cpp
+++ nncpp.cpp
@@ -31,3 +31,46 @@ double Perceptron::sigmoid(double x)
 {
     return (1.0 / (1.0 + exp(-x)));
 }
+
+MultiLayerPerceptron::MultiLayerPerceptron(vector<int> layers, double bias, double eta)
+{
+    this->layers = layers;
+    this->bias = bias;
+    this->eta = eta;
+    for (int i = 0; i < layers.size(); i++)
+    {
+        values.push_back(vector<double>(layers[i], 0.0));
+        // initialising the first neuron with 0.0
+        network.push_back(vector<Perceptron>());
+        if (i > 0)
+        // network[0] is the input layer so it has no neurons
+        {
+            for (int j = 0; j < layers[i]; j++)
+            {
+                network[i].push_back(Perceptron(layers[i - 1], bias));
+            }
+        }
+    }
+}
+
+void MultiLayerPerceptron::setWeights(vector<vector<vector<double>>> wInit)
+{
+}
+
+void MultiLayerPerceptron::printWeights()
+{
+    cout << endl;
+    for (int i = 1; i < network.size(); i++)
+    {
+        for (int j = 0; j < layers[i]; j++)
+        {
+            cout << "Layer" << i + 1 << "Neuron" << j << ": ";
+            for (auto &it : network[i][j].weights)
+            {
+                cout << it << " ";
+            }
+            cout << endl;
+        }
+    }
+    cout << endl;
+}
--- nncpp.h
+++ nncpp.h
@@ -19,3 +19,24 @@ public:
     void setWeights(vector<double> wInit);
     double sigmoid(double x);
 };
+
+class MultiLayerPerceptron
+{
+public:
+    MultiLayerPerceptron(vector<int> layers, double bias = 1.0, double eta = 0.5);
+    // eta is the learning rate of the machine-learning algorithm
+    void setWeights(vector<vector<vector<double>>> wInit);
+    void printWeights();
+    vector<double> run(vector<double> x);
+    double bp(vector<double> x, vector<double> y);
+
+    vector<int> layers;
+    double bias;
+    double eta;
+    vector<vector<Perceptron>> network;
+    // the class that consists of the actual network
+    vector<vector<double>> values;
+    // holding the output values
+    vector<vector<double>> d;
+    // used for error handling and redundant terms.
+};

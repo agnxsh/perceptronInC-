07541c920ef3aeed165afcaa498bd470fd07c8cf
Agnish Ghosh <iamagnix@gmail.com>
2022-09-04 11:11:32 +0530

    testing complete

--- NeuralNetworks.cpp
+++ NeuralNetworks.cpp
@@ -19,4 +19,15 @@ int main()
     cout << p->run({0, 1}) << endl;
     cout << p->run({1, 0}) << endl;
     cout << p->run({1, 1}) << endl;
+
+    cout << "\n\n---------Hardcoded XOR Example-----------\n\n";
+    MultiLayerPerceptron mlp = MultiLayerPerceptron({2, 2, 1});
+    mlp.setWeights({{{-10, -10, 15}, {15, 15, -10}}, {{10, 10, -15}}});
+    mlp.printWeights();
+
+    cout << "XOR: " << endl;
+    cout << "0 0 = " << mlp.run({0, 0})[0] << endl;
+    cout << "0 1 = " << mlp.run({0, 1})[0] << endl;
+    cout << "1 0 = " << mlp.run({1, 0})[0] << endl;
+    cout << "1 1 = " << mlp.run({1, 1})[0] << endl;
 }
